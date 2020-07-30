93d6550477345a0917c1b9519633480b149ac8ee
DMLooter <verbanmike@gmail.com>
2020-07-29 20:51:30 -0500

    Organized old files

---
+++ 4-bit LCD tests/4BitLCDsubroutineLoop.s
@@ -0,0 +1,123 @@
+PORTB = $4000
+PORTA = $4001
+DDRB  = $4002
+DDRA  = $4003
+
+E  = %00000100
+RW = %00000010
+RS = %00000001
+
+	.org $8000			; org defines a memory location for the next instruction. Note that the binary file will still start at $0000
+reset:
+	lda #%11111111		; Set all pins to output.
+	sta DDRB
+
+
+	lda #(%0010 << 4)			; partial initial function set for 4-bit mode, cant use subroutine
+	sta PORTB
+	ora #E 						; toggle E bit on then send to port b
+	sta PORTB
+	and #(~E)					; and !E with A register to turn the enable bit off.
+	sta PORTB
+
+	lda #%00101000				; function set for 4-bit Mode
+	jsr lcd_instruction
+
+	lda #%00001110				; display on, cursor on
+	jsr lcd_instruction
+
+	lda #%00000110				; Entry mode increment cursor, dont shift display
+	jsr lcd_instruction
+
+	ldy #0			; Initialize y offest register
+print:
+	lda message,y
+	beq loop		; If a register is zero, branch equal will jump out of the print loop.
+	jsr print_char
+	iny
+	jmp print
+
+loop:
+	jmp loop
+
+message: .asciiz "Hello, world"	; Puts null byte (x00) art end of string.
+
+lcd_wait:
+	pha				; store A reg on stack
+	lda #%00001111	; Set high four of port B to input
+	sta DDRB
+lcd_busy:			; start loop
+	lda #RW			; Set read bit
+	sta PORTB
+	lda #(RW | E)	; Toggle enable bit on.
+	sta PORTB
+	ldx PORTB		; Read bits	into x register
+	lda #RW			; Toggle enable off
+	sta PORTB
+
+	lda #(RW | E)	; Do second "read"
+	sta PORTB
+
+	txa				; put the read data back to the a register.
+	and #%10000000	; zero out all but the busy flag. Will set processor zero flag if the busy flag is zero.
+	bne lcd_busy	; If processor zero flag is not set, busy flag IS set, and we loop back
+
+	lda #RW			; reset enable low
+	sta PORTB
+	lda #%11111111	; Set port B back to output
+	sta DDRB
+	pla				; pull A register back from stack
+	rts
+
+lcd_instruction:				; This assumes the full 8-bit instruction is stored in the a register
+	jsr lcd_wait
+	tax							; Copy the instruction into the x register.
+	and #$f0					; select top four bits of command
+	sta PORTB					; put command on output, RS = 0, R/w = 0
+	ora #E						; toggle Enable.
+	sta PORTB
+	and #(~E)
+	sta PORTB
+
+	txa							; transfer the full command back into the a register
+	and #$0f					; select low four bits
+	asl							; Shift those four bits to the high four.
+	asl
+	asl
+	asl
+	sta PORTB					; set the datalines to the lcd
+	ora #E						; toggle Enable bit to write
+	sta PORTB
+	and #(~E)
+	sta PORTB
+	rts							; return from subroutine
+
+print_char:						; This assumes the 8-bit ASCII Character to print is in the a register
+	jsr lcd_wait
+	tax
+	and #$f0					; Select the top four bits of the cahracter
+	ora #RS						; Or RS to the a register
+	sta PORTB
+	ora #E						; Toggle enable bit.
+	sta PORTB
+	and #(~E)
+	sta PORTB
+
+	txa							; transfer the full command back into the a register
+	and #$0f					; select low four bits
+	asl							; Shift those four bits to the high four.
+	asl
+	asl
+	asl
+	ora #RS						; Or RS to the a register
+	sta PORTB					; set the datalines to the lcd
+	ora #E						; toggle Enable bit to write
+	sta PORTB
+	and #(~E)
+	sta PORTB
+	rts							; return from subroutine
+
+
+	.org $fffc ; $7ffc on the eeprom
+	.word reset; puts a literal value in the binary file (or the location of a label)
+	.word $0000 ; padding to make bin file right size
---
+++ 8-bit LCD tests/LCDsubroutineLoop.s
@@ -0,0 +1,87 @@
+PORTB = $6000
+PORTA = $6001
+DDRB  = $6002
+DDRA  = $6003
+
+E  = %10000000
+RW = %01000000
+RS = %00100000
+
+	.org $8000		; org defines a memory location for the next instruction. Note that the binary file will still start at $0000
+reset:
+	lda #%11111111	; set all Port b pins to output.
+	sta DDRB		;
+
+	lda #%11100000	; only the top three pins of Port A are needed for output
+	sta DDRA
+
+	lda #%00111000	; Function set(001), 8-bit(1), 2-line(1), 5x8(1), **(00).
+	jsr lcd_instruction
+
+	lda #%00001110	; Display on/off(00001), Display on(1), Cursor on(1), Blinking off(0)
+	jsr lcd_instruction
+
+	lda #%00000110	; Entry Mode Set(000001), Increment Cursor on write(1), Don't shift display(0)
+	jsr lcd_instruction
+	lda #%00000001	; Clear Display
+	jsr lcd_instruction
+
+	ldx #0			; Initialize x offest register
+print:
+	lda message,x
+	beq loop		; If a register is zero, branch equal will jump out of the print loop.
+	jsr print_char
+	inx
+	jmp print
+
+loop:
+	jmp loop
+
+message: .asciiz "Hello, world"	; Puts null byte (x00) art end of string.
+
+lcd_wait:
+	pha				; store A reg on stack
+	lda #%00000000	; Set port B to input
+	sta DDRB
+lcd_busy:			; start loop
+	lda #RW
+	sta PORTA
+	lda #(RW | E)	; pulse Enable high
+	sta PORTA
+	lda PORTB
+	and #%10000000	; zero out all but busy flag. Will set zero flag if the busy flag is zero.
+	bne lcd_busy	; If zero flag is not set, busy flag IS set, and we loop back
+
+	lda #RW			; reset enable low
+	sta PORTA
+	lda #%11111111	; Set port B back to output
+	sta DDRB
+	pla				; pull A register back from stack
+	rts
+
+lcd_instruction:
+	jsr lcd_wait
+	sta PORTB
+	lda #0 			; clear RS/RW/E bits
+	sta PORTA
+	lda #E 			; toggle E bit on, then off to send instruction
+	sta PORTA
+	lda #0 			; clear RS/RW/E bits
+	sta PORTA
+	rts				; return from subroutine
+
+
+print_char:
+	jsr lcd_wait
+	sta PORTB
+	lda #RS			; set RS to data
+	sta PORTA
+	lda #(RS | E)	; toggle E bit on, then off to send instruction
+	sta PORTA
+	lda #RS			; set RS to data and E off
+	sta PORTA
+	rts				;return
+
+	.org $fffc ; $7ffc on the eeprom
+	.word reset; puts a literal value in the binary file (or the location of a label)
+	.word $0000 ; padding to make bin file right size
